815912b14e707d4b84d4b424efaac7fd365189b6
qingemeng <qingemeng@gmail.com>
2017-02-14 11:07:41 +0530

    <Gemeng,Ranchana> | implement computeArea method properly

--- rectangle/src/main/Rectangle.java
+++ rectangle/src/main/Rectangle.java
@@ -1,10 +1,14 @@
 package main;
 
 public class Rectangle {
+
+    private double width, length;
     public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
     }
 
     public double computeArea() {
-        return 50;
+        return width * length;
     }
 }
